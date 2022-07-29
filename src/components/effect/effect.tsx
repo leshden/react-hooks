49f473bac30e55443d7d309ce95dba9e30f3d8aa
Viktor Denikin <leshden@yandex.ru>
2022-07-29 22:20:26 +0300

    code-block

--- src/components/effect/effect.tsx
+++ src/components/effect/effect.tsx
@@ -1,4 +1,7 @@
 import Button from '../button/Button';
+import {useEffect} from 'react';
+import { CodeBlock, dracula } from "react-code-blocks";
+import './Effect.css'
 
 const Effect = () => {
 
@@ -6,9 +9,28 @@ const Effect = () => {
     console.log('click!');
   }
 
+  useEffect(()=>{
+    console.log('start effect');
+  }, [])
+
+  const strCode = `const Effect = () => {
+    useEffect(()=>{}, [])
+    return (<p> рисуем компонент Effect </p>)
+  }`;
+
   return(
     <>
-      <p>useEffect</p>
+      <h1>useEffect</h1>
+      <p><strong>useEffect</strong> вызываем внути компонента (у нас компонент называется <strong>Effect</strong>)</p>
+      <div className='code-effect'>
+        <CodeBlock
+        text={strCode}
+        language={'jsx'}
+        theme={dracula}
+        wrapLines={true}
+        />
+    </div>
+
       <Button onClick={handleClick}> Click </Button>
     </>
   )
