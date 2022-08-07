7b9a7924aa4aaadc78099b3421bff35e3a98ccaf
Viktor Denikin <leshden@yandex.ru>
2022-08-07 19:21:24 +0300

    test utils

--- src/components/header/Header.test.js
+++ src/components/header/Header.test.js
@@ -2,7 +2,6 @@ import {render, screen} from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import Header from './Header';
 import {ThemeContextProvider} from '../../contexts/theme-context/ThemeContext';
-import {convertHexToRGBA} from '../../utils/Utils';
 
 test('render Header', () => {
   render(
