9ddfb1d8de903f994b6ec412a2df4e2ac212b58d
Viktor Denikin <leshden@yandex.ru>
2022-08-27 00:01:26 +0300

    add react-router-dom

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -9,12 +9,12 @@ const Menu = ({isDark}:Props) => {
   const classes = `menu-link ${isDark ? 'dark-theme' : 'light-theme'}`;
 
   return (
-  <nav className="menu-container">
-    <ul className = 'menu-ul'>
-      <li><a className ={classes} href="#">useState</a></li>
-      <li><a className ={classes} href="#">useEffect</a></li>
-    </ul>
-  </nav>
+    <nav className="menu-container">
+      <ul className = 'menu-ul'>
+        <li><a className ={classes} href="#">useState</a></li>
+        <li><a className ={classes} href="#">useEffect</a></li>
+      </ul>
+    </nav>
   )
 }
 
