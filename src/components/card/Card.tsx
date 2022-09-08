d7f39e3433ab72bb84e850f7cda38cff17821946
Viktor Denikin <leshden@yandex.ru>
2022-09-08 13:24:41 +0300

    click to path

---
+++ src/components/card/Card.tsx
@@ -0,0 +1,23 @@
+import { useNavigate } from "react-router-dom";
+import './Card.css';
+
+type Props = {
+  title: string,
+  path: string
+}
+
+const Card = ({title, path}: Props) => {
+  const navigate = useNavigate();
+
+  const handleOnClcik = () => {
+    navigate(`/${path}`);
+  }
+
+  return (
+    <div className='card-hook' onClick={handleOnClcik}>
+      {title}
+    </div>
+  )
+}
+
+export default Card;
--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,12 +1,12 @@
+import Card from '../card/Card';
 import './Home.css';
 
 const Home = () => {
   return (
     <section className='home-container'>
       <div className='card-hook-container'>
-        <div className='card-hook'>
-          useEffect
-        </div>
+        <Card title='useEffect' path='effect' />
+        <Card title='useState' path='state' />
       </div>
       <footer className='home-footer'>
         Какая-то информация об авторском праве или может
