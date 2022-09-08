b8e78b85dfab419fc2affb86251fa4cd4ec4a931
Viktor Denikin <leshden@yandex.ru>
2022-09-08 14:50:30 +0300

    useState part 1

---
+++ src/code/use-state/CodeStrings.js
@@ -0,0 +1,6 @@
+export const howInitUseState = `
+const StateComponent = () => {
+  const [counter, setCounter] = useState(0);
+  return <div>{counter}</div>
+}
+`;
--- src/components/state/State.tsx
+++ src/components/state/State.tsx
@@ -1,6 +1,35 @@
+import {useState} from 'react';
+import { CodeBlock, dracula } from "react-code-blocks";
+import {howInitUseState} from '../../code/use-state/CodeStrings';
+import './State.css';
+
 const State = () => {
+  const [counter, setCounter] = useState(0);
+  const handleOnClcik = () => {
+    setCounter(counter + 1);
+  }
+
   return (
-    <p>State</p>
+    <section className='state-container'>
+      <button onClick={handleOnClcik}>Rerender component</button>
+      <p>Когда нам нужно сохранить сотояние между перересовкой компоннета (например, сохранить счетчик,
+        который увеличивается при тапе на кнопку), то для таких целей подходит хук <strong>useState</strong>. Хук указываем в начале компонента. </p>
+      <div className='code-state'>
+        <CodeBlock
+        text={howInitUseState}
+        language={'jsx'}
+        theme={dracula}
+        wrapLines={true}
+        />
+      </div>
+      <p>
+        <strong>counter</strong> - название нашей переменной <br/>
+        <strong>setCounter</strong> - название функции, которая будет менять наш <strong>counter</strong> <br/>
+        <strong>0</strong> - инитим наш <strong>counter</strong> <br/>
+      </p>
+      <p> Обновим счетчик через клик по кнопке. Как только пользователь нажал на кнопку, счетчик обновился,
+      мы говорим <strong>React</strong> что надо перерисовать компоннет. </p>
+    </section>
   )
 }
 
