77762c7d8549da748d783f5659ca8e651586de90
Viktor Denikin <leshden@yandex.ru>
2022-09-12 18:54:40 +0300

    useState render dialog 2

--- src/components/render-state/RenderState.tsx
+++ src/components/render-state/RenderState.tsx
@@ -2,28 +2,57 @@ import {useState} from 'react'
 import Button from '../button/Button';
 import './RenderState.css';
 
-const RenderState = () => {
-  const [showCounter, setShowCounter] = useState(0);
+type Props = {
+  msec?: number;
+}
+
+const RenderState = ({msec = 1000}:Props) => {
+  const [wave, setWave] = useState<{right: string, transition?: string}>({"right": "100%"})
   const [counter, setCounter] = useState(0);
+  const [disabledBtn, setDisabledBtn] = useState(false);
   const handleOnClcik = () => {
-    setCounter(counter + 1);
+    setDisabledBtn(true)
+    setWave({"right": "50%", "transition": "right 1s ease-in"})
+    setTimeout(()=>{
+      setCounter(counter + 1);
+      setWave({"right": "100%"})
+      setDisabledBtn(false);
+    }, msec)
   }
 
+  const handleClickReset = () => {
+    setWave({"right": "100%"});
+    setCounter(0);
+  }
+
+  const idle = 'В ожидании приказа'
+  const work = 'React, стейт изменился, перерисуй мне компонет StateComponent'
+
   return (
+    <>
     <section className='canvas-container'>
       <article className='render-container'>
-        <p className='render-title-container'>
+        <div className='browser-wave' style={wave}></div>
+        <div className='render-title-container'>
           Браузер
-        </p>
+        </div>
         <div className='render-body-container'>
+          StateComponent
           <div>{counter}</div>
-          <Button onClick={handleOnClcik}> Update Counter </Button>
+          <Button onClick={handleOnClcik} disabled={disabledBtn}> Update Counter </Button>
         </div>
       </article>
-      <article className='state-react-container'>
-        Re-render Me!
+      <article className='render-container'>
+        <div className='render-title-container'>
+          Реакт
+        </div>
+        <div className='render-body-container'>
+          {disabledBtn ? work : idle}
+        </div>
       </article>
     </section>
+    <Button onClick={handleClickReset} disabled={disabledBtn}> Reset </Button>
+    </>
   )
 }
 
