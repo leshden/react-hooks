b5cc7a88dafcf3f3baa71207babee621b08a8975
Viktor Denikin <leshden@yandex.ru>
2022-09-12 21:32:14 +0300

    Updates

---
+++ static/js/react-syntax-highlighter_languages_refractor_ini.5a093de9.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_hooks=self.webpackChunkreact_hooks||[]).push([[6495],{3409:function(t){function e(t){t.languages.ini={comment:/^[ \t]*[;#].*$/m,selector:/^[ \t]*\[.*?\]/m,constant:/^[ \t]*[^\s=]+?(?=[ \t]*=)/m,"attr-value":{pattern:/=.*/,inside:{punctuation:/^[=]/}}}}t.exports=e,e.displayName="ini",e.aliases=[]}}]);
+//# sourceMappingURL=react-syntax-highlighter_languages_refractor_ini.5a093de9.chunk.js.map
