d0c43a9a80e6b349c49b1bc1b3e6d00aa8cdba56
Viktor Denikin <leshden@yandex.ru>
2022-09-28 21:32:56 +0300

    site example tree components

--- src/components/context/Context.tsx
+++ src/components/context/Context.tsx
@@ -7,6 +7,26 @@ const Context = () => {
     <p>Хук <strong>Context</strong> позволяет хранить глобальные параметры и получать доступ к этим параметров из любого компонета.
     Например, у нас есть сайт, который состоит из разных компонентов</p>
     <ExampleSite />
+    <p>И получаем дерево компоннетов этого сайта</p>
+
+    <div className='example-site-dom-line'>
+    	<div className='example-site-dom-node'> App </div>
+    </div>
+
+    <div className='example-site-dom-line'>
+    	<div className='example-site-dom-node'> Header </div>
+      <div className='example-site-dom-node'> Body </div>
+    </div>
+
+    <div className='example-site-dom-line'>
+    	<div className='example-site-dom-node'> Logo </div>
+      <div className='example-site-dom-node'> Menu </div>
+      <div className='example-site-dom-node'> Search </div>
+      <div className='example-site-dom-node'> SidePanel </div>
+      <div className='example-site-dom-node'> Content </div>
+    </div>
+
+
     </>
   )
 }
